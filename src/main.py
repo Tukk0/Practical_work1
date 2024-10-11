969cfca6a97f0f3285735480aa706eee34924c62
Tikhon <listov.ta@phystech.su>
2024-10-11 15:28:50 +0300

    Minor documentation and code readability improvements

--- src/main.py
+++ src/main.py
@@ -1,2 +1,3 @@
-from Function import function, regex_is_correct
-function("ab + c.aba. * .bac. + . + *", 'a', 2)
+from function import regex_is_correct
+from function import regex_has_a_special_prefix as checker
+checker("ab + c.aba. * .bac. + . + *", 'a', 2)
